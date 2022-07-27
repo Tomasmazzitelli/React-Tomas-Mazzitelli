426eaa84bb9edea69fe7e5385a54518bf14cabae
MacarenaAbregu <103336385+MacarenaAbregu@users.noreply.github.com>
2022-07-26 23:34:41 -0300

    Segundo tp

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./Header"
+
+
+function App(){
+  return(
+<header>
+  <main>
+    <h2>Home</h2>
+  </main>
+  <footer>
+    <p>Footer</p>
+  </footer>
+</header>
+  )
+}
+
+
+export default App
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+function Header ()
+{
+    return(<header   id="header"> 
+
+<h1   className="titulo">  Titulo</h1>
+
+<nav>
+    <a href="a">Link   </a>
+    <a href="a">Link   </a>
+</nav>
+
+
+
+    </header>
+
+    )
+}
+
+export default Header
